66d0eabf8cb6d87f9152c994a1e7da5d2c91f59f
erik <ewleon@gmail.com>
2015-10-28 15:53:21 -0400

    Commented and reorganized code.

--- public/js/event-calendar.js
+++ public/js/event-calendar.js
@@ -1,61 +1,6 @@
 $(document).ready(function () {
 
-    var $template = $('#events-template');
-    var $eventsDiv = $('#events');
-    var $addEventDiv = $('#add-event');
-    var template = Handlebars.compile($template.html());
-    var $nameInput = $('#name');
-    var $datetimeInput = $('#datetime');
-    var $descriptionInput = $('#description');
-
-    var id = 0;
-    var event1 = new Event(
-        "Team Tryouts",
-        "2015-10-20",
-        "Tryouts for the programming competition team."
-    );
-
-    event1.setId(newId());
-    var event2 = new Event(
-        "Halloween",
-        "2015-10-31",
-        "Spooky."
-    );
-    event2.setId(newId());
-    var eventList = new EventList([
-        event1,
-        event2
-    ]);
-
-
-    $addEventDiv.on('click', 'button#add', function (e) {
-        var name = $nameInput.val();
-        var datetime = $datetimeInput.val();
-        var description = $descriptionInput.val();
-
-        try {
-            var event = new Event(name, datetime, description);
-            eventList.addEvent(event)
-            $nameInput.val('');
-            $datetimeInput.val('');
-            $descriptionInput.val('');
-            render();
-        } catch (error) {
-            alert(error);
-            $datetimeInput.get(0).focus();
-        }
-    });
-
-    $eventsDiv.on('click', '.event-remove button', function (e) {
-        var id = $(this).data('eventid');
-        eventList.removeEvent(id);
-        render();
-    });
-
-    function newId() {
-        return ++id;
-    }
-
+    /* Constructor for individual event */
     function Event(name, datetime, description) {
         var self = this;
 
@@ -86,39 +31,109 @@ $(document).ready(function () {
         self.setDescription(description);
     }
 
+    /* Constructor for list of events */
     function EventList(events) {
         var self = this;
-        self.events = events;
+        self.events = [];
+
+        // Initializing ID for events
+        var id = 0;
 
         self.addEvent = function (event) {
             id++;
             event.setId(id);
             self.events.push(event);
-            render();
         };
 
         self.removeEvent = function (id) {
             self.events = self.events.filter(function (event) {
                 return id != event.id;
             });
-            render();
         };
 
         self.sort = function () {
             self.events.sort(function (event1, event2) {
-                return event1.dateObj < event2.dateObj;
+                return event2.dateObj < event1.dateObj;
             });
         };
+
+        events.forEach(function (event) {
+            self.addEvent(event);
+        })
     }
 
+    /* Cache DOM */
+    // Template for events list
+    var $template = $('#events-template');
 
+    // Compiling into Handlebars template object
+    var template = Handlebars.compile($template.html());
+
+    // Section that receives rendered template
+    var $eventsDiv = $('#events');
+
+    // Section for adding new events
+    var $addEventDiv = $('#add-event');
+    var $nameInput = $('#name');
+    var $datetimeInput = $('#datetime');
+    var $descriptionInput = $('#description');
+
+    /* Creating initial events to display */
+    var event1 = new Event(
+        "Team Tryouts",
+        "2015-10-20",
+        "Tryouts for the programming competition team."
+    );
+
+    var event2 = new Event(
+        "Halloween",
+        "2015-10-31",
+        "Spooky."
+    );
+
+    var eventList = new EventList([
+        event1,
+        event2
+    ]);
+
+    /* Event handlers */
+    // Handler for adding new events 
+    $addEventDiv.on('click', 'button#add', function (e) {
+        var name = $nameInput.val();
+        var datetime = $datetimeInput.val();
+        var description = $descriptionInput.val();
+
+        try {
+            var event = new Event(name, datetime, description);
+            eventList.addEvent(event)
+            $nameInput.val('');
+            $datetimeInput.val('');
+            $descriptionInput.val('');
+            render();
+        } catch (error) {
+            alert(error);
+            $datetimeInput.get(0).focus();
+        }
+    });
+
+    // Handler for removing individual events 
+    $eventsDiv.on('click', '.event-remove button', function (e) {
+        var id = $(this).data('eventid');
+        eventList.removeEvent(id);
+        render();
+    });
+
+
+    /* Render the page with the current data */
     function render() {
+        eventList.sort();
         var context = {
             events: eventList.events
         };
         $eventsDiv.html(template(context));
     }
 
+    /* Initial render */
     render();
 
 });
